1ca95eec45f6935af580da5a18882ef73e71e452
Juhee <pjh402941@gmail.com>
2025-08-25 14:21:26 +0900

    chore: storybook 설정 및 파일 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -9,11 +12,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: ['src/api/orval/**/*'],
-  },
-];
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
+  ignores: ['src/api/orval/**/*'],
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
